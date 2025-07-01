01f87931d82a43c1c51d4ed900257cc90101254b
dharmraj <sodhadharmraj30@gmail.com>
2025-07-01 16:02:55 +0530

    Q5 to Q7 in tutorial 1

--- LAB1/Q5.cs
+++ LAB1/Q5.cs
@@ -1,24 +1,36 @@
-﻿
-using System;
+﻿using System;  // Importing the System namespace for basic input/output operations
+
+// Declaring a namespace to group related classes
 namespace _24SOECE13043_Dharmraj_sodha.LAB1
 {
-
+    // Class definition
     class Q5
-{
-    static void Main(string[] args)
     {
-        int n, fact = 1;
-        Console.WriteLine("Enter Number : ");
-        string str = Console.ReadLine();
-        n = Convert.ToInt32(str);
-        for (int i = 1; i <= n; i++)
+        // Main method - entry point of the program
+        static void Main(string[] args)
         {
-            fact = fact * i;
-        }
-        Console.WriteLine("Factorial : {0}", fact);
+            int n, fact = 1; // Declare variables: n for input number, fact to store factorial result
 
-        Console.Read();
+            // Prompt user to enter a number
+            Console.WriteLine("Enter Number : ");
+
+            // Read user input as string
+            string str = Console.ReadLine();
+
+            // Convert the input string to an integer
+            n = Convert.ToInt32(str);
+
+            // Calculate factorial using a for loop
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i; // Multiply current value of fact by i
+            }
+
+            // Display the calculated factorial
+            Console.WriteLine("Factorial : {0}", fact);
+
+            // Keep the console window open until a key is pressed
+            Console.Read();
+        }
     }
 }
-}
-
---
+++ LAB1/Q6.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _24SOECE13043_Dharmraj_sodha.LAB1
+
+{
+
+    class Q6
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int a, b, c, result;
+
+            Console.Write("Enter Number   1: ");
+
+            string str = Console.ReadLine();
+
+            a = Convert.ToInt32(str);
+
+
+
+            Console.Write("Enter Number  2 : ");
+
+            str = Console.ReadLine();
+
+            b = Convert.ToInt32(str);
+
+
+
+            Console.Write("Enter Number   3 : ");
+            str = Console.ReadLine()
+            c = Convert.ToInt32(str);
+            result = Sum(a, b, c);
+
+            Console.Write($"Sum of above numbers is: {result}");
+            Console.Read();
+
+        }
+
+        static int Sum(int x, int y, int z)
+
+        {
+
+            int res;
+
+            res = x + y + z;
+
+            return res;
+
+        }
+
+    }
+
+}
