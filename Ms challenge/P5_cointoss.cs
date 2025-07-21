bd028a7e0a33fb8c06324e2d1a83c98734c12129
dharmraj <sodhadharmraj30@gmail.com>
2025-07-21 16:33:24 +0900

    Ms Challenges

---
+++ Ms challenge/P5_cointoss.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13043_Dharmraj_sodha.Ms_challenge
+{
+    internal class P5_cointoss
+    {
+        public static void Main(string[] args)
+        {
+            
+            Random random = new Random();
+            Console.WriteLine($"Coin toss result is {((random.Next(0, 2)== 0)?"Heads":"Tails")} ");
+            Console.WriteLine($"Coin toss result is {((random.Next(0, 2) == 0) ? "Heads" : "Tails")} ");
+            Console.WriteLine($"Coin toss result is {((random.Next(0, 2) == 0) ? "Heads" : "Tails")} ");
+
+        }
+    }
+}
---
+++ Ms challenge/P6_adminAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13043_Dharmraj_sodha.Ms_challenge
+{
+    internal class P6_adminAccess
+    {
+        public static void Main(String[] args) 
+        {
+            Random random = new Random();
+            int i = random.Next(0, 3);
+            string permission = ((i == 0) ? "Admin" :((i == 1)? "Manager": "User"));
+            int level = random.Next(0,61);
+            Console.WriteLine($"Current user : {permission} on Level {level}");
+            Console.WriteLine($"{((permission == "Admin") ? ((level > 55) ? "Welcome, Super Admin user." : "Welcome, Admin user.") : ((permission == "Manager") ? ((level > 20) ? "Contact an Admin for access." : "You do not have sufficient privileges.") : "You do not have sufficient privileges."))}");
+
+        }
+    }
+}
